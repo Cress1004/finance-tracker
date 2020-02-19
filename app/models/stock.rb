ff88595827950ba2ec5e97455a829fd235765f6e
Thanh <nuhoangchenhac@gmail.com>
2020-02-19 11:36:40 +0700

    User-stock : many to many association

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: 'Tpk_0cfd064d968a4ec5bb980db4edcc818c',
@@ -8,5 +12,9 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
