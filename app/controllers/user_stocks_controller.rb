2578b827f0c6237a64e8543e9349596c6d042bd8
Thanh <nuhoangchenhac@gmail.com>
2020-02-26 20:09:49 +0700

    Remove ticker

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -10,4 +10,12 @@ class UserStocksController < ApplicationController
     flash[:notice]="Stock #{stock.name} was successfully added to your portfilio"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id)
+    user_stock.first.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from portfilio"
+    redirect_to my_portfolio_path
+  end
 end
